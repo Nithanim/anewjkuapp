0e75248ba17b0ee73b5d30d682a08309b0f03cb7
marunjar@aon.at <marunjar@aon.at>
2014-11-23 10:49:19 +0100

    apply last changes
    pt 7: courses

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/LvaListAdapter.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/LvaListAdapter.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.voidsink.anewjkuapp.R;
+import org.voidsink.anewjkuapp.base.StickyArrayAdapter;
 import org.voidsink.anewjkuapp.calendar.CalendarUtils;
 import org.voidsink.anewjkuapp.kusss.ExamGrade;
 import org.voidsink.anewjkuapp.kusss.LvaState;
 import org.voidsink.anewjkuapp.kusss.LvaWithGrade;
 import org.voidsink.anewjkuapp.utils.AppUtils;
+import org.voidsink.anewjkuapp.utils.UIUtils;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,88 +21,31 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
-public class LvaListAdapter extends BaseExpandableListAdapter implements
-		ListAdapter {
+public class LvaListAdapter extends StickyArrayAdapter<LvaWithGrade>{
 
-	private List<LvaWithGrade> mDoneLvas;
-	private List<LvaWithGrade> mOpenLvas;
 	private List<LvaWithGrade> mAllLvas;
 	private LayoutInflater inflater;
-	private Context mContext;
 
-	public LvaListAdapter(Context context, List<LvaWithGrade> doneLvas,
-			List<LvaWithGrade> openLvas) {
-		this.inflater = LayoutInflater.from(context);
-		this.mContext = context;
-
-		this.mDoneLvas = doneLvas;
-		this.mOpenLvas = openLvas;
-
-		this.mAllLvas = new ArrayList<LvaWithGrade>();
-		this.mAllLvas.addAll(this.mDoneLvas);
-		this.mAllLvas.addAll(this.mOpenLvas);
-
-		AppUtils.sortLVAsWithGrade(this.mAllLvas);
-		AppUtils.sortLVAsWithGrade(this.mDoneLvas);
-		AppUtils.sortLVAsWithGrade(this.mOpenLvas);
+	public LvaListAdapter(Context context) {
+		super(context, 0);
+        this.inflater = LayoutInflater.from(context);
 	}
 
-	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+        LvaWithGrade item = this.getItem(position);
+        if (item !=null) {
+            view = getLvaView(convertView, parent, item);
+        } else {
+            view = null;
+        }
+        return view;
+    }
 
-	@Override
-	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getItemId(int position) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getItemViewType(int position) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getViewTypeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
-
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		return getLvaList(groupPosition).get(childPosition);
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		LvaWithGrade lva = getLvaList(groupPosition).get(childPosition);
+	public View getLvaView(View convertView, ViewGroup parent,
+                           LvaWithGrade item) {
+		LvaWithGrade lva = item;
 		LvaList2ItemHolder lvaHolder = null;
 
 		if (convertView == null) {
@@ -107,20 +53,16 @@ public class LvaListAdapter extends BaseExpandableListAdapter implements
 					false);
 			lvaHolder = new LvaList2ItemHolder();
 
-			lvaHolder.title = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_title);
-			lvaHolder.lvaNr = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_lvanr);
-			lvaHolder.skz = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_skz);
-			lvaHolder.type = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_type);
-			lvaHolder.chip = (View) convertView
-					.findViewById(R.id.lva_list2_chip);
-			lvaHolder.ects = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_ects);
-			lvaHolder.code = (TextView) convertView
-					.findViewById(R.id.lva_list2_item_code);
+			lvaHolder.title = (TextView) convertView.findViewById(R.id.lva_list2_item_title);
+            lvaHolder.lvaNr = (TextView) convertView.findViewById(R.id.lva_list2_item_lvanr);
+            lvaHolder.skz = (TextView) convertView.findViewById(R.id.lva_list2_item_skz);
+            lvaHolder.code = (TextView) convertView.findViewById(R.id.lva_list2_item_code);
+            lvaHolder.teacher = (TextView) convertView.findViewById(R.id.lva_list2_item_teacher);
+
+            lvaHolder.chipBack = (View) convertView.findViewById(R.id.grade_chip);
+            lvaHolder.chipEcts = (TextView) convertView.findViewById(R.id.grade_chip_info);
+            lvaHolder.chipGrade = (TextView) convertView.findViewById(R.id.grade_chip_grade);
+
 
 			convertView.setTag(lvaHolder);
 		}
@@ -129,120 +71,75 @@ public class LvaListAdapter extends BaseExpandableListAdapter implements
 			lvaHolder = (LvaList2ItemHolder) convertView.getTag();
 		}
 
-		ExamGrade grade = lva.getGrade();
-		if (grade == null) {
-			lvaHolder.chip.setBackgroundColor(CalendarUtils.COLOR_DEFAULT_LVA);
-		} else {
-			lvaHolder.chip.setBackgroundColor(grade.getGrade().getColor());
-		}
+        lvaHolder.title.setText(lva.getLva().getTitle());
+        UIUtils.setTextAndVisibility(lvaHolder.teacher, lva.getLva().getTeacher());
+        lvaHolder.lvaNr.setText(lva.getLva().getLvaNr());
+        lvaHolder.skz.setText(String.format("[%s]", lva.getLva().getSKZ()));
+        lvaHolder.code.setText(lva.getLva().getCode());
 
-		lvaHolder.lvaNr.setText(lva.getLva().getLvaNr());
-		lvaHolder.title.setText(lva.getLva().getTitle());
-		lvaHolder.skz.setText(String.format("[%s]", lva.getLva().getSKZ()));
-		lvaHolder.type.setText(lva.getLva().getLvaType());
-		lvaHolder.ects.setText(String.format("%.2f ECTS", lva.getLva()
-				.getEcts()));
-		lvaHolder.code.setText(lva.getLva().getCode());
+        ExamGrade grade = lva.getGrade();
+        if (grade == null) {
+            lvaHolder.chipBack.setBackgroundColor(Color.GRAY);
+            lvaHolder.chipGrade.setText("?");
+        } else {
+            lvaHolder.chipBack.setBackgroundColor(grade.getGrade().getColor());
+            lvaHolder.chipGrade.setText(String.format("%d", grade.getGrade().getValue()));
+        }
+        lvaHolder.chipEcts.setText(String.format("%.2f ECTS", lva.getLva()
+                .getEcts()));
 
 		return convertView;
-	}
-
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		return getLvaList(groupPosition).size();
-	}
-
-	@Override
-	public Object getGroup(int groupPosition) {
-		return getLvaList(groupPosition);
-	}
-
-	private List<LvaWithGrade> getLvaList(int groupPosition) {
-		switch (groupPosition) {
-		case 0:
-			return this.mDoneLvas;
-		case 1:
-			return this.mOpenLvas;
-		case 2:
-			return this.mAllLvas;
-		default:
-			return null;
-		}
-	}
-
-	private CharSequence getGroupTitle(int groupPosition) {
-		switch (groupPosition) {
-		case 0:
-			return mContext.getString(LvaState.DONE.getStringResID());
-		case 1:
-			return mContext.getString(LvaState.OPEN.getStringResID());
-		case 2:
-			return mContext.getString(LvaState.ALL.getStringResID());
-		default:
-			return null;
-		}
-	}
-
-	@Override
-	public int getGroupCount() {
-		return 3;
-	}
-
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
-	}
-
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		LvaList2GroupHolder groupHolder = null;
-
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.lva_list_group, parent,
-					false);
-			groupHolder = new LvaList2GroupHolder();
-			groupHolder.term = (TextView) convertView
-					.findViewById(R.id.lva_list2_group_term);
-			groupHolder.ects = (TextView) convertView
-					.findViewById(R.id.lva_list2_group_ects);
-			convertView.setTag(groupHolder);
-		}
-
-		if (groupHolder == null) {
-			groupHolder = (LvaList2GroupHolder) convertView.getTag();
-		}
-
-		groupHolder.term.setText(getGroupTitle(groupPosition));
-		groupHolder.ects.setText(String.format("%.2f ECTS",
-				AppUtils.getECTS(LvaState.ALL, getLvaList(groupPosition))));
-		return convertView;
-	}
-
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		return false;
-	}
-
-	@Override
-	public boolean isEnabled(int position) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	private class LvaList2GroupHolder {
-		private TextView term;
-		private TextView ects;
 	}
 
 	private static class LvaList2ItemHolder {
-		public View chip;
-		public TextView type;
-		private TextView title;
-		private TextView lvaNr;
-		private TextView skz;
-		private TextView ects;
-		private TextView code;
+        private TextView title;
+        private TextView lvaNr;
+        private TextView skz;
+        private TextView code;
+        private TextView teacher;
+        private View chipBack;
+        private TextView chipEcts;
+        private TextView chipGrade;
+
 	}
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public View getHeaderView(int position, View convertView, ViewGroup viewGroup) {
+        // Build your custom HeaderView
+        LayoutInflater mInflater = LayoutInflater.from(getContext());
+        final View headerView = mInflater.inflate(R.layout.list_header, null);
+        final TextView tvHeaderTitle = (TextView) headerView.findViewById(R.id.list_header_text);
+
+        LvaWithGrade card = getItem(position);
+        if (card instanceof LvaWithGrade) {
+            tvHeaderTitle.setText(getContext().getString(((LvaWithGrade) card).getState().getStringResID()));
+        }
+        return headerView;
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+        LvaWithGrade card = getItem(position);
+        if (card instanceof LvaWithGrade) {
+            return ((LvaWithGrade) card).getState().getStringResID();
+        }
+        return 0;
+    }
+
 
 }
--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/LvaDetailFragment.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/LvaDetailFragment.java
@@ -1,232 +1,71 @@
 package org.voidsink.anewjkuapp.fragment;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
+
+import org.voidsink.anewjkuapp.LvaListAdapter;
+import org.voidsink.anewjkuapp.R;
+import org.voidsink.anewjkuapp.base.BaseFragment;
+import org.voidsink.anewjkuapp.kusss.ExamGrade;
+import org.voidsink.anewjkuapp.kusss.Lva;
+import org.voidsink.anewjkuapp.kusss.LvaWithGrade;
+import org.voidsink.anewjkuapp.utils.AppUtils;
+import org.voidsink.anewjkuapp.view.StickyListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.voidsink.anewjkuapp.kusss.LvaState;
-import org.voidsink.anewjkuapp.utils.AppUtils;
-import org.voidsink.anewjkuapp.LvaListAdapter;
-import org.voidsink.anewjkuapp.PreferenceWrapper;
-import org.voidsink.anewjkuapp.R;
-import org.voidsink.anewjkuapp.base.BaseFragment;
-import org.voidsink.anewjkuapp.kusss.ExamGrade;
-import org.voidsink.anewjkuapp.kusss.Grade;
-import org.voidsink.anewjkuapp.kusss.Lva;
-import org.voidsink.anewjkuapp.kusss.LvaWithGrade;
-
-import com.androidplot.pie.PieChart;
-import com.androidplot.ui.XLayoutStyle;
-import com.androidplot.ui.XPositionMetric;
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.BarFormatter;
-import com.androidplot.xy.BarRenderer;
-import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYStepMode;
-import com.androidplot.xy.YValueMarker;
-
 @SuppressLint("ValidFragment")
 public class LvaDetailFragment extends BaseFragment {
 
-	private static final String TAG = LvaDetailFragment.class.getSimpleName();
+    private static final String TAG = LvaDetailFragment.class.getSimpleName();
 
-	private List<String> mTerms;
+    private List<String> mTerms;
+    private LvaListAdapter mAdapter;
 
-	private List<LvaWithGrade> mOpenLvas;
-	private List<LvaWithGrade> mDoneLvas;
-	private ExpandableListView mExpListView;
+    private List<LvaWithGrade> mLvas = new ArrayList<>();
 
-	private LvaListAdapter mAdapter;
-
-	public LvaDetailFragment() {
-		this("", new ArrayList<Lva>(), new ArrayList<ExamGrade>());
-	}
-
-	public LvaDetailFragment(List<String> terms, List<Lva> lvas,
-			List<ExamGrade> grades) {
-		this.mTerms = terms;
-
-		this.mOpenLvas = new ArrayList<LvaWithGrade>();
-		this.mDoneLvas = new ArrayList<LvaWithGrade>();
-
-		// Log.i(TAG, this.mTerms.toString());
-		for (Lva lva : lvas) {
-			if (mTerms.contains(lva.getTerm())) {
-				ExamGrade grade = findGrade(grades, lva);
-				if (grade == null || grade.getGrade() == Grade.G5) {
-					this.mOpenLvas.add(new LvaWithGrade(lva, grade));
-					// Log.i(TAG, "open: " + lva.getKey() + " - " +
-					// lva.getECTS());
-				} else {
-					this.mDoneLvas.add(new LvaWithGrade(lva, grade));
-					// Log.i(TAG, "done: " + lva.getKey() + " - " +
-					// lva.getECTS());
-				}
-			}
-		}
-		// remove duplicates
-		AppUtils.removeDuplicates(this.mDoneLvas, this.mOpenLvas);
-	}
-
-	public LvaDetailFragment(String term, List<Lva> lvas, List<ExamGrade> grades) {
-		this(Arrays.asList(new String[] { term }), lvas, grades);
-	}
-
-	private ExamGrade findGrade(List<ExamGrade> grades, Lva lva) {
-		ExamGrade finalGrade = null;
-
-		for (ExamGrade grade : grades) {
-			if (grade.getCode().equals(lva.getCode())) {
-				if (finalGrade == null || finalGrade.getGrade() == Grade.G5) {
-					finalGrade = grade;
-				}
-			}
-		}
-		if (finalGrade == null) {
-			Log.d(TAG, "findByLvaNr: " + lva.getLvaNr() + "/" + lva.getTitle());
-			for (ExamGrade grade : grades) {
-				if (grade.getLvaNr().equals(lva.getLvaNr())) {
-					if (finalGrade == null || finalGrade.getGrade() == Grade.G5) {
-						finalGrade = grade;
-					}
-				}
-			}
-		}
-
-		return finalGrade;
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_lva_detail, container,
-				false);
-
-		double mDoneEcts = AppUtils.getECTS(LvaState.DONE, mDoneLvas);
-		double mOpenEcts = AppUtils.getECTS(LvaState.OPEN, mOpenLvas);
-
-		mExpListView = (ExpandableListView) view
-				.findViewById(R.id.lva_lists);
-		mAdapter = new LvaListAdapter(getContext(), this.mDoneLvas,
-				this.mOpenLvas);
-		mExpListView.setAdapter((ExpandableListAdapter) mAdapter);
-
-		double minEcts = mTerms.size() * 30;
-
-		XYPlot barChart = (XYPlot) view.findViewById(R.id.stat_card_lva_diagram_bar);
-		PieChart pieChart = (PieChart) view.findViewById(R.id.stat_card_lva_diagram_pie);
-
-		if (PreferenceWrapper.getUseLvaBarChart(getContext())) {
-			pieChart.setVisibility(View.GONE);
-
-			YValueMarker ectsMarker = new YValueMarker(minEcts, // y-val to mark
-					String.format("%.2f ECTS", minEcts), // marker label
-					new XPositionMetric( // object instance to set text
-											// positioning
-											// on the marker
-							PixelUtils.dpToPix(5), // 5dp offset
-							XLayoutStyle.ABSOLUTE_FROM_RIGHT), // offset origin
-					Color.rgb(220, 0, 0), // line paint color
-					Color.rgb(220, 0, 0)); // text paint color
-
-			ectsMarker.getTextPaint().setTextSize(PixelUtils.dpToPix(12));
-
-			DashPathEffect dpe = new DashPathEffect(new float[] {
-					PixelUtils.dpToPix(2), PixelUtils.dpToPix(2) }, 0);
-
-			ectsMarker.getLinePaint().setPathEffect(dpe);
-
-			// calc range manually
-			double rangeTopMax = mTerms.size() * 30;
-			if (mDoneEcts + mOpenEcts > (rangeTopMax * .9)) {
-				rangeTopMax = (Math.ceil((mDoneEcts + mOpenEcts) * 1.1 / 10) * 10);
-			}
-
-			// calc steps
-			double rangeStep = Math.ceil((rangeTopMax / 10) / 10) * 10;
-
-			// init bar chart
-            addSerieToBarChart(barChart, getContext().getString(LvaState.DONE.getStringResIDExt()),
-                    mDoneEcts, Grade.G1.getColor());
-            addSerieToBarChart(barChart, getContext().getString(LvaState.OPEN.getStringResIDExt()),
-                    mOpenEcts, Grade.G3.getColor());
-
-			barChart.setRangeTopMin(mTerms.size() * 30);
-			barChart.setRangeBoundaries(0, BoundaryMode.FIXED, rangeTopMax,
-					BoundaryMode.FIXED);
-			barChart.setRangeStep(XYStepMode.INCREMENT_BY_VAL, rangeStep);
-
-			// workaround to center ects bar
-			barChart.setDomainBoundaries(0, 2, BoundaryMode.FIXED);
-			barChart.addMarker(ectsMarker);
-
-			// Setup the BarRenderer with our selected options
-			BarRenderer<?> renderer = ((BarRenderer<?>) barChart
-					.getRenderer(BarRenderer.class));
-			if (renderer != null) {
-				renderer.setBarRenderStyle(BarRenderer.BarRenderStyle.STACKED);
-				renderer.setBarWidthStyle(BarRenderer.BarWidthStyle.VARIABLE_WIDTH);
-				renderer.setBarGap(25);
-			}
-
-			if (barChart.getSeriesSet().size() > 0) {
-				barChart.setVisibility(View.VISIBLE);
-			} else {
-				barChart.setVisibility(View.GONE);
-			}
-
-		} else {
-			barChart.setVisibility(View.GONE);
-
-			// init pie chart
-            addSerieToBarChart(barChart, getContext().getString(LvaState.DONE.getStringResIDExt()),
-                    mDoneEcts, Grade.G1.getColor());
-            addSerieToBarChart(barChart, getContext().getString(LvaState.OPEN.getStringResIDExt()),
-                    mOpenEcts, Grade.G3.getColor());
-
-			if (pieChart.getSeriesSet().size() > 0) {
-				pieChart.setVisibility(View.VISIBLE);
-			} else {
-				pieChart.setVisibility(View.GONE);
-			}
-		}
-
-		return view;
-	}
-
-    private void addSerieToBarChart(XYPlot barChart, String category,
-                                    double value, int color) {
-        if (value > 0) {
-            List<Number> values = new ArrayList<Number>();
-            values.add(null); // workaround to center ects bar
-            values.add(value);
-            values.add(null); // workaround to center ects bar
-
-            SimpleXYSeries mSeries = new SimpleXYSeries(values,
-                    SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, category);
-            barChart.addSeries(mSeries,
-                    new BarFormatter(color, Color.GRAY));
-        }
+    public LvaDetailFragment() {
+        this("", new ArrayList<Lva>(), new ArrayList<ExamGrade>());
     }
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		super.onCreateOptionsMenu(menu, inflater);
-		inflater.inflate(R.menu.lva, menu);
-	}
+    public LvaDetailFragment(List<String> terms, List<Lva> lvas,
+                             List<ExamGrade> grades) {
+        super();
+
+        this.mTerms = terms;
+        this.mLvas = AppUtils.getLvasWithGrades(terms, lvas, grades);
+    }
+
+    public LvaDetailFragment(String term, List<Lva> lvas, List<ExamGrade> grades) {
+        this(Arrays.asList(new String[]{term}), lvas, grades);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_card_lva_detail, container,
+                false);
+
+        final StickyListView mListView = (StickyListView) view.findViewById(R.id.lva_card_list);
+
+        mAdapter = new LvaListAdapter(getContext());
+        AppUtils.sortLVAsWithGrade(mLvas);
+        mAdapter.addAll(mLvas);
+        mListView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.lva, menu);
+    }
 }
