e9d165e1e0af32771a934ea4bfe7762877d64ae9
marunjar@aon.at <marunjar@aon.at>
2014-12-07 16:46:31 +0100

    update api version
    api version 21

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/NavigationDrawerFragment.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/NavigationDrawerFragment.java
@@ -5,7 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.accounts.Account;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
@@ -222,17 +222,9 @@ public class NavigationDrawerFragment extends BaseFragment {
 		// ActionBarDrawerToggle ties together the the proper interactions
 		// between the navigation drawer and the action bar app icon.
 		mDrawerToggle = new ActionBarDrawerToggle(getActivity(), /* host Activity */
-		mDrawerLayout, /* DrawerLayout object */
-		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
-		R.string.navigation_drawer_open, /*
-										 * "open drawer" description for
-										 * accessibility
-										 */
-		R.string.navigation_drawer_close /*
-										 * "close drawer" description for
-										 * accessibility
-										 */
-		) {
+	    	    mDrawerLayout, /* DrawerLayout object */
+    		    R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+                R.drawable.ic_drawer) {
 			@Override
 			public void onDrawerClosed(View drawerView) {
 				super.onDrawerClosed(drawerView);
